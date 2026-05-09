a0d9c61f688a818379608237bab8571997e5539c
Petr Pivoňka <petrpiv1@gmail.com>
2026-05-09 03:01:16 +0200

    Revert configure services

--- BlazorWasmOfflineSample/Program.cs
+++ BlazorWasmOfflineSample/Program.cs
@@ -6,11 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
-
-static void ConfigureServices(IServiceCollection services, string baseAddress)
-{
-    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
-}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
